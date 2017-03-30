70d3d87256a9943d6d7eb2a6d17f6770bb04d71c
xzywww <xzywwwqwer@gmail.com>
2017-03-30 22:37:42 +0800

    webgl05-update3

--- tasks/webgl05/webgl05.js
+++ tasks/webgl05/webgl05.js
@@ -142,6 +142,11 @@
         });
     }
 
+    //帧数监视器
+    let stats = new Stats();
+    stats.showPanel(0); // 0: fps, 1: ms, 2: mb, 3+: custom
+    document.body.appendChild( stats.dom );
+
     //主渲染函数
     const render = function () {
         //1 控制汽车移动
@@ -150,6 +155,9 @@
         renderer.render(scene, camera);
         //3 更新控制器
         controller.update();
+        //4 更新帧数显示器
+        stats.begin();
+        stats.end();
         requestAnimationFrame(render);
     }
 
