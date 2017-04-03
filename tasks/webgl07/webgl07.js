7b5859048b6a692c69ea3c053c88130e9a9ff297
xzywww <xzywwwqwer@gmail.com>
2017-04-03 22:12:39 +0800

    webgl07-reBuild

--- tasks/webgl07/webgl07.js
+++ tasks/webgl07/webgl07.js
@@ -1,47 +1,47 @@
 /**
  * Created by xzywww on 2017-04-02.
  */
-(function () {
-    let renderer;
-    const initRender = function () {
-        renderer = new THREE.WebGLRenderer();
-        renderer.setSize(window.innerWidth, window.innerHeight);
-        document.body.appendChild(renderer.domElement);
-        //设置阴影
-        renderer.shadowMap.enabled = false;
-        //平滑阴影
-        renderer.shadowMap.type = THREE.PCFSoftShadowMap;
-    };
+;(function (window, Rx) {
+    class Main{
+        constructor(){
+            this.initlization();
+        }
+        initlization(){
+            this.initRender();
+            this.initScene();
+            this.initLight();
+        }
+        initRender(){
+            this.renderer = new THREE.WebGLRenderer();
+            this.renderer.setSize(window.innerWidth, window.innerHeight);
+            document.body.appendChild(renderer.domElement);
+            //设置阴影
+            this.renderer.shadowMap.enabled = false;
+            //平滑阴影
+            this.renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+        }
+        initScene() {
+            this.scene = new THREE.Scene();
+        }
+        initLight(){
+            const globalLight = function () {
+                this.scene.add(new THREE.AmbientLight(0xffffff, 0.3));
+            }
 
-    //场景
-    let scene;
-    const initScene = function () {
-        scene = new THREE.Scene();
-    };
+            //z轴正向点灯光
+            const zLight = function () {
+                let light = new THREE.SpotLight(0xffffff, 1);
+                light.position.set(-50, 50, 100);
+                light.castShadow = true;
+                this.scene.add(light);
+            }
 
-    //灯光
-    const initLight = function (scene) {
-        //全局白光
-        const globalLight = function () {
-            scene.add(new THREE.AmbientLight(0xffffff, 0.3));
+            globalLight();
+            zLight();
         }
 
-        //z轴正向点灯光
-        const zLight = function () {
-            let light = new THREE.SpotLight(0xffffff, 1);
-            light.position.set(-50, 50, 100);
-            light.castShadow = true;
-            scene.add(light);
-        }
-
-        globalLight();
-        zLight();
     }
+    new Main();
+})(window);
 
-    const init = function () {
-        initRender();
-        initScene();
-        initLight();
-    }
-    window.initThree = init
-})()
+
