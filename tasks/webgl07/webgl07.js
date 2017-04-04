9a1ee8dccc825bd828f41cab4879ba992ef88ba6
xzywww <xzywwwqwer@gmail.com>
2017-04-04 09:47:50 +0800

    webgl07-update

--- tasks/webgl07/webgl07.js
+++ tasks/webgl07/webgl07.js
@@ -2,28 +2,36 @@
  * Created by xzywww on 2017-04-02.
  */
 ;(function (window, THREE) {
-    class Main{
-        constructor(){
+    //初始化程序
+    class Main {
+        constructor() {
             this.initlization();
         }
-        initlization(){
+
+        initlization() {
             this.initRender();
             this.initScene();
             this.initLight();
+            this.initCamera();
+            this.initController();
         }
-        initRender(){
+
+        initRender() {
             this.renderer = new THREE.WebGLRenderer();
             this.renderer.setSize(window.innerWidth, window.innerHeight);
             document.body.appendChild(this.renderer.domElement);
             //设置阴影
             this.renderer.shadowMap.enabled = false;
             //平滑阴影
-            this.renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+            //this.renderer.shadowMap.type = THREE.PCFSoftShadowMap;
         }
+
         initScene() {
             this.scene = new THREE.Scene();
         }
-        initLight(){
+
+        initLight() {
+            let that = this;
             let _scene = this.scene;
 
             const globalLight = function (scene) {
@@ -32,18 +40,68 @@
 
             //z轴正向点灯光
             const zLight = function (scene) {
-                let light = new THREE.SpotLight(0xffffff, 1);
-                light.position.set(-50, 50, 100);
-                light.castShadow = true;
-                scene.add(light);
+                that.light = new THREE.SpotLight(0xffffff, 1);
+                that.light.position.set(-50, 50, 100);
+                that.light.castShadow = true;
+                scene.add(that.light);
             }
 
             globalLight(_scene);
             zLight(_scene);
         }
 
+        initCamera() {
+            //镜头
+            this.camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 1, 3000);
+            this.camera.position.set(50, 50, 50);
+            this.camera.lookAt(new THREE.Vector3(0, 0, 0));
+            this.scene.add(this.camera);
+        }
+
+        initController() {
+            this.controller = new THREE.TrackballControls(this.camera, this.renderer.domElement);
+            this.controller.rotateSpeed = 6.0;
+        }
+
     }
-    new Main();
+
+    //测试物品
+    let testObj = new Main();
+
+    (function (obj) {
+        //加载着色器
+        let material = new THREE.ShaderMaterial( {
+
+            uniforms: {
+                color:{
+                    type: 'v3',
+                    value: new THREE.Color('#60371b')
+                },
+                light: {
+                    type: 'v3',
+                    value: (new THREE.Vector3(300,100,100))
+                }
+            },
+            vertexShader: document.getElementById( 'vertexShader' ).textContent,
+            fragmentShader: document.getElementById( 'fragmentShader' ).textContent
+
+        } );
+
+        let geometry = new THREE.OctahedronBufferGeometry(60, 2);
+        let cube = new THREE.Mesh(geometry, material);
+        cube.castShadow = true;
+        obj.scene.add(cube);
+    })(testObj);
+
+    //循环渲染
+    const start = function () {
+        testObj.renderer.render(testObj.scene, testObj.camera);
+        testObj.controller.update();
+        requestAnimationFrame(start);
+    }
+
+    start();
+
 })(window, THREE);
 
 
